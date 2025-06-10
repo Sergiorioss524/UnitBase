d819d4bd79344e0e9c0416110706aa8598c73b03
Sergio Rios Genuzio <64295026+Sergiorioss524@users.noreply.github.com>
2025-06-10 13:27:12 +0200

    fix: clean up supabase client

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,8 +1,8 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js';
-import { env } from '~/env';
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { env } from "~/env";
 
 export const createClient = () =>
   createSupabaseClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  ); 
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  );
